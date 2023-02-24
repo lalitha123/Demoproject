1391fb153890773c499908b0d4a95fcea21a4200
lalitha <lalitha.mycareer@gmail.com>
2023-02-24 17:20:15 +0530

    new branch code

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Demo Project</title>
-</head>
-<body>
-
-</body>
-</html>
+<h1>New branch edit</h1>
